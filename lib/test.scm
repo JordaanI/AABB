d53b1fce24cfe73424c383c13db7f929b8215251
JordaanI <jordaanivan@gmail.com>
2025-12-21 15:08:15 -0500

    tests updated

--- lib/test.scm
+++ lib/test.scm
@@ -1,9 +1,19 @@
-(define b0 (new-boundry '(0 0) '(1 1)))
-(define b1 (new-boundry '(0 0) 1))
-(define b2 (new-boundry '(1 1) '(2 2)))
-(define b3 (new-boundry '(2 2) 0.75))
+(define b0 (new-boundry '(2 2) '(1 1)))
+(define b1 (new-boundry '(2 -2) '(1 1)))
+(define b2 (new-boundry '(-2 2) '(1 1)))
+(define b3 (new-boundry '(-2 -2) '(1 1)))
 
-(pp (collision? b0 b1))
-(pp (collision? b2 b1))
-(pp (collision? b2 b0))
-(pp (collision? b1 b3))
+(define b4 (new-boundry '(0 0) 0.5))
+(define b5 (new-boundry '(2 2) '(1 3)))
+
+(define boundries (list b0 b1 b2 b3 #;b4 #;b5))
+
+(pp boundries)
+
+(define tree (apply build-tree boundries))
+
+(pp tree)
+
+(define collisions (boundry-collisions tree b4))
+
+(pp collisions)
